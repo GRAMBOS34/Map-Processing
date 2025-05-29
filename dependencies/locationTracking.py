75d936483d93f0be1971b883f84bdbd0bbceb36e
GRAMBOS34 <vladimirivanjumawan@gmail.com>
2025-05-29 16:27:15 +0800

    Removed redundant checks

--- dependencies/locationTracking.py
+++ dependencies/locationTracking.py
@@ -9,12 +9,6 @@ class locationTrack:
     # Gets the distance between two points
     def getDistance (self, coords1:tuple, coords2:tuple) -> float:
         
-        # Checks if the given coordinates are only within two dimensions
-        # Just in case
-        if len(coords1) != 2 or len(coords2) != 2:
-            # this line pretty much stops the program because it throws an error
-            raise Exception("Coordinates don't exist, try adding or removing a value because this function only works with two dimensions")
-        
         # Calculate the distance
         xDistComponent = abs(coords2[0] - coords1[0]) # Get the absolute value since we only need distance and not direction
         yDistComponent = abs(coords2[1] - coords1[1]) # Same here
@@ -27,12 +21,6 @@ class locationTrack:
     # the math will be explained within the function
     # coords will be taken as a tuple of two integers
     def getTurnAngle(self, previousPointCoords:tuple, currentPointCoords:tuple, nextPointCoords:tuple) -> float:
-
-        # Checks if the given coordinates are only within two dimensions
-        # Just in case
-        if len(previousPointCoords) != 2 or len(currentPointCoords) != 2 or len(nextPointCoords) != 2:
-            # this line pretty much stops the program because it throws an error
-            raise Exception("Coordinates don't exist, try adding or removing a value because this function only works with two dimensions")
         
         # Get distances between coordinates
         distA = locationTrack.getDistance(self, previousPointCoords, currentPointCoords)
